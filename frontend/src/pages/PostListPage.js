c3082d9a3c252f6fc2c26b8ee0f952e01c41e189
leesh5000 <leesh50000@gmail.com>
2022-03-13 20:48:16 +0900

    temp commit

---
+++ frontend/src/pages/PostListPage.js
@@ -0,0 +1,13 @@
+import Header from '../components/common/Header';
+import HeaderContainer from '../containers/common/HeaderContainer';
+
+const PostListPage = () => {
+  return (
+    <>
+      <HeaderContainer />
+      <div>안녕하세요.</div>
+    </>
+  );
+};
+
+export default PostListPage;
